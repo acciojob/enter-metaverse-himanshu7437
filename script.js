a2cfa55fc8e8f81b52de79d1b1e1d23e038885a1
Himanshu Sharma <143724283+himanshu7437@users.noreply.github.com>
2025-11-05 12:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let status = document.querySelector("#status");
+let enterBtn = document.querySelector("#enterBtn");
+
+enterBtn.addEventListener("click", () => {
+	status.innerHTML = `<h1>${status.textContent}</h1>`
+})
