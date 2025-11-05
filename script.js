5c6017d4fbd09ae4563eeac2819b702a4378a094
Himanshu Sharma <143724283+himanshu7437@users.noreply.github.com>
2025-11-05 13:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let status = document.querySelector("#status");
 let enterBtn = document.querySelector("#enterBtn");
 
 enterBtn.addEventListener("click", () => {
-	status.innerHTML = `<h1>${status.textContent}</h1>`
+	status.innerHTML = `<h1>Entered Metaverse</h1>`
 })
